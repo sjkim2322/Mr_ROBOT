d4ae90a3b6573c8d646222674a45e51c3f9bf6df
Thomas Yoo <thomasyoo@Thomass-MacBook-Pro.local>
2017-04-28 18:39:08 +0900

    Mouse 로 이벤트 처리

--- extension/getSource.js
+++ extension/getSource.js
@@ -10,4 +10,6 @@ function get_element(mydocument)
   });
 }
 
-document.onmouseover = get_element(this);
+var body = document.getElementsByTagName('body');
+body[0].innerHTML = body[0].innerHTML + '<script src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.10.3/jquery-ui.min.js"></script>';
+body[0].innerHTML = body[0].innerHTML + '<script>$("*").on("mouseover",function(){console.log($(this))});</script>';
