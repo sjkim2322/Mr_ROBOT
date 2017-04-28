33e731c1214ff960359734c43b09c76910600142
KIMSEONGJIN <sjkim2322@naver.com>
2017-04-28 18:33:36 +0900

    Extension Default File 추가

---
+++ extension/getSource.js
@@ -0,0 +1,13 @@
+function get_source(document_body){
+    return document_body.name;
+}
+function get_element(mydocument)
+{
+  console.log("!!!");
+  chrome.extension.sendMessage({
+      action: "getSource",
+      source: get_source(mydocument)
+  });
+}
+
+document.onmouseover = get_element(this);
